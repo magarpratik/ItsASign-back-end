36e0c5b052ec7552046521b8d57b052abfff3ffc
NatassaV <annbonni@gmail.com>
2022-01-24 11:44:19 +0000

    setup mogoose

--- backend/connection.js
+++ backend/connection.js
@@ -1,1 +1,22 @@
-//const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const { DB_PASS } = process.env;
+const { URL } = process.env;
+const { DB_USER } = process.env;
+
+const options = {
+  user: DB_USER,
+  pass: DB_PASS,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+mongoose
+  .connect(URL, options)
+  .then(() => {
+    console.log("database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
