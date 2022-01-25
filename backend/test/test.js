4ef5839d2ef3352959b94c415ccace93c35db6c4
Josh-Human <jhumanb1@gmail.com>
2022-01-25 12:56:00 +0000

    GET users test passing

---
+++ backend/test/test.js
@@ -0,0 +1,49 @@
+/* eslint-env node, mocha */
+/* eslint-disable no-unused-vars */
+require('dotenv').config({ path: `${__dirname}/.env.test` });
+const mongoose = require('mongoose');
+const { expect } = require('chai');
+const should = require('chai').should();
+const request = require('supertest');
+const app = require('../app');
+
+mongoose.Promise = global.Promise;
+
+before((done) => {
+  const { DB_PASS } = process.env;
+  const { URL } = process.env;
+  const { DB_USER } = process.env;
+
+  const options = {
+    user: DB_USER,
+    pass: DB_PASS,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  mongoose.connect(URL, options);
+  mongoose.connection
+    .once('open', () => {
+      done();
+    })
+    .on('error', (error) => {
+      console.warn('warning', error);
+      done(error);
+    });
+});
+// beforeEach((done) => {
+//     done();
+// });
+after(() => {
+  mongoose.disconnect();
+});
+
+describe('GET /api/users', (done) => {
+  it('200: returns list of all users', () => request(app)
+    .get('/api/users')
+    .expect(200)
+    .then((res) => {
+      const { users } = res.body;
+      users.should.have.lengthOf(12);
+    }));
+});
