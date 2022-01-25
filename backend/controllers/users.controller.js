e04af71702ea92256b87384aa2f52d7a1bdabd98
Josh-Human <jhumanb1@gmail.com>
2022-01-25 16:55:24 +0000

    user tests created

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,14 +1,14 @@
-const { Users } = require("../models/users.model");
+const { Users } = require('../models/users.model');
 
 exports.getUsers = (req, res) => {
-    Users.find()
-        .sort("createdAt")
-        .then((users) => {
-            res.status(200).send({ users });
-        })
-        .catch((err) => {
-            res.status(400).send(
-                `There was an error with loading Users. ${err}`
-            );
-        });
+  Users.find()
+    .sort('createdAt')
+    .then((users) => {
+      res.status(200).send({ users });
+    })
+    .catch((err) => {
+      res.status(400).send(
+        `There was an error with loading Users. ${err}`,
+      );
+    });
 };
