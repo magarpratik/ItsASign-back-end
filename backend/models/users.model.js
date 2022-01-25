14ab120ccfaa7dd8668b1b379754cd9973f9d301
Josh-Human <jhumanb1@gmail.com>
2022-01-25 15:47:32 +0000

    updated User schema

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -1,21 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
 const userSchema = new Schema(
-  {
-    username: { type: Object, required: true },
-    picture: { type: String },
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: Number, required: true },
-  },
-  {
-    timestamps: true,
-  },
+    {
+        username: { type: Object, required: true },
+        picture: { type: String },
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        password: { type: Number, required: true },
+        progress: {
+            completed_lessons: { type: Array },
+            total_xp: { type: Number },
+            badges: { type: Array },
+        },
+    },
+    {
+        timestamps: true,
+    }
 );
 
-const Users = mongoose.model('User', userSchema);
+const Users = mongoose.model("User", userSchema);
 // converting the raw table into a workable model
 // with model(<name>, <schemaUsed>)
 
