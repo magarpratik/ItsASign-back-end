cd79e1d99006af125fd9195e88d811e607b4c327
Josh-Human <jhumanb1@gmail.com>
2022-02-02 15:32:30 +0000

    added route for ranked users

--- backend/routers/api.router.js
+++ backend/routers/api.router.js
@@ -3,11 +3,11 @@ const apiRouter = require("express").Router();
 const usersRouter = require("./users.router");
 
 const coursesRouter = require("./lessons.router");
-const { SignIn } = require("../controllers/users.controller");
+const { SignIn, getRankedUsers } = require("../controllers/users.controller");
 const res = require("express/lib/response");
 
 apiRouter.use("/users", usersRouter);
 apiRouter.use("/courses", coursesRouter);
 apiRouter.get("/sign_in/", SignIn);
-
+apiRouter.get("/ranked_users", getRankedUsers);
 module.exports = apiRouter;
